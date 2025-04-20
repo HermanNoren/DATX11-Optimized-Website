2be805829e512c45a2ea34b75e5364d8cedd18c1
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 00:57:51 +0200

    added components to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import React from "react";
 import AboutTitle from "./_components/AboutTitle";
+import ScrollRevealText from "./_components/ScrollRevealText";
 
 export default function AboutPage() {
     return (
       <section>
         <AboutTitle/>
+        <ScrollRevealText/>
       </section>
     );
 }
