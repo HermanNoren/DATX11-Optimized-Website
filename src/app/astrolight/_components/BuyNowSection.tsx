dd8146a448920d39cc8c73b796b84b35c27b6d1e
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 21:19:44 +0200

    fixed footer and some minor layout issues

--- src/app/astrolight/_components/BuyNowSection.tsx
+++ src/app/astrolight/_components/BuyNowSection.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function BuyNowSection() {
   return (
-    <section className="w-full py-[5em] flex justify-center items-center">
+    <section className="w-full py-[5em] flex justify-center items-center gradient-bg relative z-10">
       <div className="relative w-full h-[30em] overflow-hidden flex items-center justify-center">
         <video
           className="absolute top-[50%] translate-y-[-50%] left-0 w-full h-[20em] object-cover z-0"
@@ -28,7 +28,7 @@ export default function BuyNowSection() {
           </h2>
         </div>
 
-        <div className="relative z-10 bg-[#d9d9d9] w-full max-w-md p-[3em] text-center flex flex-col gap-[1.5em]">
+        <div className="relative z-10 bg-[#d9d9d9] h-[25em] w-full max-w-md p-[3em] justify-center text-center flex flex-col gap-[1.5em]">
           <h2 className="uppercase text-lg font-normal">
             LIMITED. ICONIC. UNEARTHLY.
           </h2>
@@ -38,7 +38,7 @@ export default function BuyNowSection() {
             This is not just a material. It is an experience, a relic, a vision cast in metal. Astrolight is the culmination of exploration, craftsmanship, and cosmic wonder.
           </p>
 
-          <div className="mt-2">
+          <div className="mt-2 px-9">
             <Button as={Link} href="/products" >BUY NOW</Button>
           </div>
         </div>
