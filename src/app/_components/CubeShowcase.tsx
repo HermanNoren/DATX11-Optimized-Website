fd28bd316f6f8f1b9c4d34d1d78e1aaea13c3f82
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 13:16:16 +0200

    swithed sides, added button, fixed spacing

--- src/app/_components/CubeShowcase.tsx
+++ src/app/_components/CubeShowcase.tsx
@@ -1,16 +1,16 @@
 import RoomScene from "@/components/3D/RoomScene/RoomScene";
+import Button from "@/components/Button";
+import Link from "next/link";
 
 
 export default function CubeShowcase() {
   return (
     <section className="relative w-full py-36 gradient-bg z-10">
-      <div className="relative container w-full h-full grid grid-cols-12 gap-8">
-        <div className="w-full aspect-[2/1.5] col-span-7">
-          <RoomScene />
-        </div>
-        <div className="flex flex-col gap-12 col-span-5">
-          <h2 className="text-6xl uppercase">Luxurious decorations</h2>
-          <p>
+      <div className="relative container w-full h-full grid grid-cols-2 is-md  gap-8">
+       
+        <div className="flex flex-col gap-6 justify-center items-end">
+          <h2 className="text-6xl uppercase text-end">Luxurious decorations</h2>
+          <p className="text-end pb-6">
             Acquiring a cube is more than a purchase, it&apos;s an entry into a world
             of luxury and refinement. Each Astrolite cube is precision-forged,
             certified, and delivered with the assurance of authenticity.
@@ -18,6 +18,10 @@ export default function CubeShowcase() {
             seamless way to own a piece of the extraordinary. In a market where
             the exceptional is never ordinary, deCube sets the standard.
           </p>
+          <Button className="w-60" as={Link} href="/about"> READ MORE</Button>
+        </div>
+        <div className="w-full aspect-[2/1.5] ">
+          <RoomScene />
         </div>
       </div>
     </section>
