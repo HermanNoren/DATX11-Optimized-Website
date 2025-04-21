d7f265ab07b753b59fe59c4e1b61493a4c71c552
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 11:01:29 +0200

    Added astrolight 3d model

--- src/app/astrolight/page.tsx
+++ src/app/astrolight/page.tsx
@@ -4,12 +4,14 @@ import React from "react";
 import AstrolightHero from "./_components/AstrolightHero";
 import BornBeyondEarth from "./_components/BornBeyondEarth";
 import FourTextSections from "./_components/FourTextSections";
+import AstrolightScene from "@/components/3D/astrolightScene/AstrolightScene";
 
 export default function AstrolightPage() {
   return (
     <>
       <AstrolightHero />
       <BornBeyondEarth />
+      <div className="w-screen h-90 "><AstrolightScene scale={0.6}/></div>
       <FourTextSections/>
     </>
   );
---
+++ src/components/3D/astrolightScene/AstrolightScene.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import {
+  CameraControls,
+  Environment,
+} from "@react-three/drei";
+import { Vector3 } from "three";
+import AstrolightModel from "./AstrolightModel";
+
+
+interface AstrolightSceneProps {
+  scale?: number;
+  position?: Vector3 | [number, number, number];
+}
+
+export default function AstrolightScene({
+  scale,
+  position,
+}: AstrolightSceneProps) {
+  return (
+    <Canvas shadows camera={{ fov: 45 }}>
+      <AstrolightModel scale={scale} position={position} />
+      <CameraControls
+        makeDefault
+        dollySpeed={0}
+        minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+      />
+      <directionalLight
+        position={[5, 5, -15]}
+        castShadow
+        intensity={50}
+        shadow-mapSize={2048}
+        shadow-bias={-0.001}
+      >
+        <orthographicCamera
+          attach="shadow-camera"
+          args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]}
+        />
+      </directionalLight>
+      <directionalLight
+        position={[5, 5, 15]}
+        castShadow
+        intensity={50}
+        shadow-mapSize={2048}
+        shadow-bias={-0.001}
+      >
+        <orthographicCamera
+          attach="shadow-camera"
+          args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]}
+        />
+      </directionalLight>
+      <Environment preset="studio" />
+    </Canvas>
+  );
+}
