1ac26f55940c7aa5c5561c109abc38b78a54c022
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 03:42:49 +0200

    added the components for astrolight in the page.tsx

---
+++ src/app/astrolight/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import AstrolightHero from "./_components/AstrolightHero";
+import BornBeyondEarth from "./_components/BornBeyondEarth";
+import FourTextSections from "./_components/FourTextSections";
+
+export default function AstrolightPage() {
+  return (
+    <>
+      <AstrolightHero />
+      <BornBeyondEarth />
+      <FourTextSections/>
+    </>
+  );
+}
