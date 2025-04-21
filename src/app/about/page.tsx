eb9d7533982114f4c6bc2997c9c15c8e96080964
jennycarlsson <44492196+jennycarlsson@users.noreply.github.com>
2025-04-21 02:11:51 +0200

    Added components to the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,18 @@
 import React from "react";
 import AboutTitle from "./_components/AboutTitle";
 import ScrollRevealText from "./_components/ScrollRevealText";
+import ParallaxImageSection from "./_components/parallaxImageSection";
+import RecievingProduct from "./_components/RecievingProduct";
+import CeoQuote from "./_components/CeoQuote";
 
 export default function AboutPage() {
     return (
       <section>
         <AboutTitle/>
         <ScrollRevealText/>
+        <ParallaxImageSection/>
+        <RecievingProduct/>
+        <CeoQuote/>
       </section>
     );
 }
