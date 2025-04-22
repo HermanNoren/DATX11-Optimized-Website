3b859319cad2d6e83cdcb54883c0c4115f1f3ac2
KingHermando <herman.noren@outlook.com>
2025-04-22 18:43:15 +0200

    Fix cube showcase text anims

--- src/app/_components/CubeShowcase.tsx
+++ src/app/_components/CubeShowcase.tsx
@@ -1,29 +1,36 @@
 import RoomScene from "@/components/3D/RoomScene/RoomScene";
 import Button from "@/components/Button";
+import ClipPathReveal from "@/components/ClipPathReveal";
+import MaskText from "@/components/MaskText";
+import ParallaxScroll from "@/components/ParallaxScroll";
 import Link from "next/link";
 
 export default function CubeShowcase() {
+  const paragraphPhrase = `Acquiring a cube is more than a purchase, it's an entry into a
+world of luxury and refinement. Each Astrolite cube is
+precision-forged, certified, and delivered with the assurance of
+authenticity. Designed for collectors, pioneers, and visionaries,
+deCube offers a seamless way to own a piece of the extraordinary. In
+a market where the exceptional is never ordinary, deCube sets the
+standard.`;
   return (
     <section className="relative w-full py-36 gradient-bg z-10">
       <div className="relative container w-full h-full grid grid-cols-2 is-md  gap-8">
         <div className="flex flex-col gap-6 justify-center items-end">
-          <h2 className="text-6xl uppercase text-end">Luxurious decorations</h2>
+          <h2 className="text-6xl uppercase text-end">
+            <MaskText phrase="Luxurious decorations" stagger={0.1} />
+          </h2>
           <p className="text-end pb-6">
-            Acquiring a cube is more than a purchase, it&apos;s an entry into a
-            world of luxury and refinement. Each Astrolite cube is
-            precision-forged, certified, and delivered with the assurance of
-            authenticity. Designed for collectors, pioneers, and visionaries,
-            deCube offers a seamless way to own a piece of the extraordinary. In
-            a market where the exceptional is never ordinary, deCube sets the
-            standard.
+            <MaskText phrase={paragraphPhrase} stagger={0.005} />
           </p>
           <Button className="w-60" as={Link} href="/about">
-            {" "}
             READ MORE
           </Button>
         </div>
         <div className="w-full aspect-[2/1.5] hover:cursor-grab">
-          <RoomScene />
+          <ClipPathReveal>
+            <RoomScene />
+          </ClipPathReveal>
         </div>
       </div>
     </section>
